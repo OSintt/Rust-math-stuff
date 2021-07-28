9f7c7dd24b83c5722cdb5da0997598cc81caf923
OSintt <akumagamer123@gmail.com>
2021-07-28 10:46:50 -0500

    an attempt to iter floatants

--- raiz.rs
+++ raiz.rs
@@ -1,9 +1,11 @@
 fn main() {
-	let numero: i64 = 100;
+	let numero: i32 = 100;
 	let mut vec = Vec::new();
 
-	for i in 0..numero {
-		if i * i == numero {
+	for i in 0..numero * 100 {
+		let i = i as f32 * 0.01;
+		println!("{:?}", i);
+		if i * i == numero as f32 {
 			println!("La raíz cuadrada de {} es {}", numero, i);
 			vec.push(i);
 			break;
